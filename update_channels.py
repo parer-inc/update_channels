4406814ffff4457739c89ef380b136f87e76443f
max <maxfy@gmail.com>
2021-06-08 13:31:24 +0300

    implemented

---
+++ update_channels.py
@@ -0,0 +1,46 @@
+"""This service allows to write new channels to db"""
+import os
+import sys
+import time
+import MySQLdb
+from rq import Worker, Queue, Connection
+from methods.connection import get_redis, get_cursor
+
+
+def update_channels(data):
+    """Updates channels in database (table channels)
+       data must be a 2d array - [n][12]"""
+    cursor, db = get_cursor()
+    if not cursor or not db:
+        # log that failed getting cursor
+        return False
+    try:
+        for chan in data:
+            if chan is None or len(chan) != 12:
+                return False
+        q = '''REPLACE INTO  channels
+                (id, title, description, custom_url,
+                published_at, default_language, views,
+                subscribers, hidden_subscribers, videos,
+                keywords, country)
+                VALUES
+                (%s, %s, %s, %s, %s, %s,
+                 %s, %s, %s, %s, %s, %s, );'''
+        cursor.executemany(q, data)
+    except Exception as error:
+        print(error)
+        # LOG
+        return False
+        # sys.exit("Error:Failed writing new chanles to db")
+    cursor.execute()
+    db.commit()
+    return True
+
+
+if __name__ == '__main__':
+    time.sleep(5)
+    r = get_redis()
+    q = Queue('update_channels', connection=r)
+    with Connection(r):
+        worker = Worker([q], connection=r,  name='update_channels')
+        worker.work()
